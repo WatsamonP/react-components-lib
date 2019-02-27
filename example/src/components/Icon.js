33f81acbf8367c7a0eebad7a79c487d5f0875653
WatsamonP <pongsupan.watsamon@gmail.com>
2019-02-27 16:35:31 +0700

    add Icon

---
+++ example/src/components/Icon.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { JoyTheme } from '../Style/JoyTheme'
+
+export const UserIcon = <i className="fa fa-user" aria-hidden="true"></i>
+export const Calendar = <i className="fa fa-calendar-o" aria-hidden="true"></i>
+export const MapMarker = <i className="fa fa-map-marker" aria-hidden="true"></i>
+export const Medkit = <i className="fa fa-medkit" aria-hidden="true"></i>
+export const FileText = <i className="fa fa-file-text" aria-hidden="true"></i>
+export const StickyNote = <i className="fa fa-sticky-note-o" aria-hidden="true"></i>
+export const BalanceScale = <i className="fa fa-balance-scale" aria-hidden="true"></i>
+export const Smile = <i className="fa fa-smile-o" aria-hidden="true"></i>
+export const FloppyIcon = <i className="fa fa-floppy-o" aria-hidden="true"></i>
+export const ClearIcon = <i className="fa fa-trash" aria-hidden="true"></i>
+export const checkCircle = <i className="fa fa-check-circle" aria-hidden="true"></i>
+export const SearchIcon = (<i className="fa fa-search" aria-hidden="true"></i>)
+export const Pencil = <i className="fa fa-pencil" aria-hidden="true"></i>
+export const Trash = <i className="fa fa-trash" aria-hidden="true"></i>
+export const Male = <i className="fa fa-male" aria-hidden="true" style={{ color: JoyTheme.LIGHT_NAVY }}></i>
+export const Female = <i className="fa fa-female" aria-hidden="true" style={{ color: JoyTheme.PINK }}></i>
+export const FaSignOut = <i className="fa fa-sign-out" aria-hidden="true"></i>
+export const FaUser = <i className="fa fa-user" aria-hidden="true"></i>
+export const Bars = <i className="fa fa-bars" aria-hidden="true"></i>
+export const Wraning = <i className="fa fa-exclamation-triangle" aria-hidden="true"></i>
+export const Wraning404 = <i className="fa fa-exclamation-triangle" style={{color: JoyTheme.GREY, fontSize: '150px'}} aria-hidden="true"></i>
+export const FaPlusCir = <i className="fa fa-plus-circle" aria-hidden="true"></i>
+export const FaList = <i className="fa fa-list" aria-hidden="true"></i>
